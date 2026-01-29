62f9831de60fbb3220c5312d20378c72b2beb282
ATTAIKI-Aymane <126726377+ATTAIKI-Aymane@users.noreply.github.com>
2026-01-29 11:01:54 +0100

    ninja ex

--- week_1/day_1/exercises/exercises_xp_ninja.js
+++ week_1/day_1/exercises/exercises_xp_ninja.js
@@ -1,9 +1,63 @@
 // ===== Exercise 1
+const person1 = {
+  fullName: "Aymane Attaiki",
+  mass: 75,      // بالكيلوغرام
+  height: 1.75,  // بالمتر
+  calcBMI: function () {
+    return this.mass / (this.height * this.height);
+  }
+};
+
+const person2 = {
+  fullName: "Ahmed Benali",
+  mass: 85,
+  height: 1.8,
+  calcBMI: function () {
+    return this.mass / (this.height * this.height);
+  }
+};
+
+function compareBMI(p1, p2) {
+  const bmi1 = p1.calcBMI();
+  const bmi2 = p2.calcBMI();
+
+  if (bmi1 > bmi2) {
+    console.log(p1.fullName + " has the higher BMI");
+  } else if (bmi2 > bmi1) {
+    console.log(p2.fullName + " has the higher BMI");
+  } else {
+    console.log("Both have the same BMI");
+  }
+}
+
+compareBMI(person1, person2);
+
 
 // ===== Exercise 2
+function calculateAverage(gradesList) {
+  let sum = 0;
 
-// ===== Exercise 3
+  for (let i = 0; i < gradesList.length; i++) {
+    sum += gradesList[i];
+  }
 
-// ===== Exercise 4
+  return sum / gradesList.length;
+}
 
-// ===== Exercise ...
+function findAvg(gradesList){
+    const average = calculateAverage(gradesList);
+
+    if (average >= 65) {
+        console.log("Passed with an average of " + average);
+}else {
+        console.log("Failed with an average of " + average);
+    }
+}
+
+const studentGrades = [80, 77, 88, 95, 68];
+const studentGrades2 = [50, 60, 58, 62, 47];
+
+findAvg(studentGrades);
+findAvg(studentGrades2);
+
+
