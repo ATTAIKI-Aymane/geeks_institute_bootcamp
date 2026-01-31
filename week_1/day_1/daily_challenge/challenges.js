c71ac2fc597c065e99a864bd202a4c642ae8b0ca
ATTAIKI-Aymane <126726377+ATTAIKI-Aymane@users.noreply.github.com>
2026-01-31 17:38:33 +0100

    first commit challenges.js

--- week_1/day_1/daily_challenge/challenges.js
+++ week_1/day_1/daily_challenge/challenges.js
@@ -1,4 +1,20 @@
-// ====== Daily Challenge 1
+// ====== Daily Challenge Not Bad ===================================================
+let sentence = "This movie is not very very bad for me";
+
+let wordNot = sentence.indexOf("not");
+let wordBad = sentence.indexOf("bad");
+
+if (wordNot !== -1 && wordBad !== -1 && wordBad > wordNot) {
+  let result =
+    sentence.slice(0, wordNot) +
+    "good" +
+    sentence.slice(wordBad + "bad".length);
+
+  console.log(result);
+} else {
+  console.log(sentence);
+}
+
 
 // ====== Daily Challenge 2
 
