3348a7d20b7eaaab366038648385d2681ad0fee5
ATTAIKI-Aymane <126726377+ATTAIKI-Aymane@users.noreply.github.com>
2026-02-01 18:10:38 +0100

    first commit

--- week_1/day_2/exercises/exercises_xp.js
+++ week_1/day_2/exercises/exercises_xp.js
@@ -1,4 +1,18 @@
 // ===== Exercise 1
+function displayNumbersDivisible(divisor) {
+  let sum = 0;
+
+  for (let i = 0; i <= 500; i++) {
+    if (i % divisor === 0) {
+      console.log(i);
+      sum += i;
+    }
+  }
+
+  console.log("Sum :", sum);
+}
+
+displayNumbersDivisible(23);
 
 // ===== Exercise 2
 
