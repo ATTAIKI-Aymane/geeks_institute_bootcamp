73104a993d3e3bc48b3a05343bdcf59f17141aa9
ATTAIKI-Aymane <126726377+ATTAIKI-Aymane@users.noreply.github.com>
2026-02-02 16:07:23 +0100

    ex 5 gold

--- week_1/day_2/exercises/exercises_xp_gold.js
+++ week_1/day_2/exercises/exercises_xp_gold.js
@@ -1,9 +1,57 @@
-// ===== Exercise 1
+// ===== Exercise 1 =======================================================================================================
+function isBlank(str) {
+  return str.length === 0;
+}
 
-// ===== Exercise 2
+// Tests
+console.log(isBlank(''));      // true
+console.log(isBlank('abc'));   // false
 
-// ===== Exercise 3
+// ===== Exercise 2 =======================================================================================================
+function abbrevName(name) {
+  const parts = name.split(" "); 
+  return parts[0] + " " + parts[1][0] + ".";
+}
 
-// ===== Exercise 4
+// Test
+console.log(abbrevName("Robin Singh")); // "Robin S."
 
-// ===== Exercise ...
+// ===== Exercise 3 =======================================================================================================
+function swapCase(str) {
+  let result = "";
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+
+    if (char === char.toUpperCase()) {
+      result += char.toLowerCase();
+    } else {
+      result += char.toUpperCase();
+    }
+  }
+
+  return result;
+}
+
+// Test
+console.log(swapCase("The Quick Brown Fox"));
+// tHE qUICK bROWN fOX
+
+// ===== Exercise 4 =======================================================================================================
+    function isOmnipresent(arr, value) {
+  for (let i = 0; i < arr.length; i++) {
+    if (!arr[i].includes(value)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// Tests
+console.log(isOmnipresent([[1, 1], [1, 3], [5, 1], [6, 1]], 1)); // true
+console.log(isOmnipresent([[1, 1], [1, 3], [5, 1], [6, 1]], 6)); // false
+
+// ===== Exercise 5 =======================================================================================================
+
+
+// to checking this exercise please check out this link"https://github.com/ATTAIKI-Aymane/geeks_institute_bootcamp/blob/main/week_1/day_2/exercises/ex_5_xp_gold.html"
