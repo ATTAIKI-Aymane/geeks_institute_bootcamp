8df03881871b886d1795fd1b84a6c035ec0d7847
ATTAIKI-Aymane <126726377+ATTAIKI-Aymane@users.noreply.github.com>
2026-02-05 14:45:56 +0100

    add ninja

--- week_1/day_2/exercises/exercises_xp_ninja.js
+++ week_1/day_2/exercises/exercises_xp_ninja.js
@@ -1,9 +1,101 @@
-// ===== Exercise 1
+// ===== Exercise 1 ===========================================================================================================
 
-// ===== Exercise 2
+// Generate random number
+let randomNum = Math.floor(Math.random() * 100) + 1;
+console.log("Random Number:", randomNum);
 
-// ===== Exercise 3
+// Loop even numbers
+for (let i = 0; i <= randomNum; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
 
-// ===== Exercise 4
+// ===== Exercise 2 ===========================================================================================================
 
-// ===== Exercise ...
+function capitalize(str) {
+
+  let evenCap = ""; // even indexes capital
+  let oddCap = "";  // odd indexes capital
+
+  for (let i = 0; i < str.length; i++) {
+
+    // Even index
+    if (i % 2 === 0) {
+      evenCap += str[i].toUpperCase();
+      oddCap  += str[i].toLowerCase();
+    } 
+    
+    // Odd index
+    else {
+      evenCap += str[i].toLowerCase();
+      oddCap  += str[i].toUpperCase();
+    }
+
+  }
+
+  return [evenCap, oddCap];
+}
+
+// ===== Exercise 3 ===========================================================================================================
+
+function isPalindrome(str) {
+
+  // reverse the string
+  let reversed = str.split("").reverse().join("");
+
+  // compare
+  if (str === reversed) {
+    return true;
+  } else {
+    return false;
+  }
+
+}
+
+
+// ===== Exercise 4 ===========================================================================================================
+
+function biggestNumberInArray(arrayNumber) {
+
+  // إذا كان array خاوي
+  if (arrayNumber.length === 0) {
+    return 0;
+  }
+
+  let biggest = -Infinity; // أصغر قيمة ممكنة
+
+  for (let i = 0; i < arrayNumber.length; i++) {
+
+    // نتحقق واش element رقم
+    if (typeof arrayNumber[i] === "number") {
+
+      if (arrayNumber[i] > biggest) {
+        biggest = arrayNumber[i];
+      }
+
+    }
+
+  }
+
+  return biggest === -Infinity ? 0 : biggest;
+}
+
+
+// ===== Exercise 5 ===========================================================================================================
+
+function getUnique(arr) {
+
+  let newList = [];
+
+  for (let i = 0; i < arr.length; i++) {
+
+    // إلا ماكانش element ف newList نضيفوه
+    if (!newList.includes(arr[i])) {
+      newList.push(arr[i]);
+    }
+
+  }
+
+  return newList;
+}
