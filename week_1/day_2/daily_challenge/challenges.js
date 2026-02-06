720dc5f3b85bfbb904244f0393e701ce1a28e984
ATTAIKI-Aymane <126726377+ATTAIKI-Aymane@users.noreply.github.com>
2026-02-06 12:02:34 +0100

    first commit planets

--- week_1/day_2/daily_challenge/challenges.js
+++ week_1/day_2/daily_challenge/challenges.js
@@ -1,5 +1,9 @@
 // ====== Daily Challenge 1
+// to checking this exercise please check out this link "week_1/day_2/daily_challenge/Planets"
+
 
 // ====== Daily Challenge 2
+// to checking this exercise please check out this link ""
+
 
 // ====== Daily Challenge 3
